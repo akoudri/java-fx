896f9439ae16d92dbf82da070638eac93fb0bc97
akoudri <ali.koudri@gmail.com>
2024-12-04 12:00:28 +0100

    Cleans CalcController implementation

--- src/main/java/com/akfc/training/gui/CalcController.java
+++ src/main/java/com/akfc/training/gui/CalcController.java
@@ -7,13 +7,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import org.nfunk.jep.JEP;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CalcController implements Initializable {
 
-    private StringBuffer number = new StringBuffer();
+    private JEP jep = new JEP();
+    private boolean resultDisplayed = false;
 
     @FXML
     private GridPane rootPane;
@@ -22,19 +24,33 @@ public class CalcController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("Calc launched");
+        // Configuration initiale de JEP
+        jep.addStandardFunctions();
+        jep.addStandardConstants();
     }
 
     @FXML
     protected void onNumberClick(ActionEvent event) {
         Button button = (Button) event.getSource();
-        number.append(button.getText());
-        display.setText(number.toString());
+        String btnText = button.getText();
+        //TODO: implements number click
     }
 
     @FXML
     protected void onOperationCall(ActionEvent event) {
-        //TODO: Implement this method
+        //TODO: implements equals click
+    }
+
+    @FXML
+    protected void onClearEvent(ActionEvent event) {
+        //TODO: clear expression
+    }
+
+    private String formatResult(double result) {
+        // Gestion de l'affichage des décimales
+        return Math.abs(result - (long) result) < 0.0000001
+                ? String.valueOf((long) result)
+                : String.valueOf(result);
     }
 
     @FXML
